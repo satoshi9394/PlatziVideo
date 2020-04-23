cb3c3153b0ca15b7e88db9511c1230aa6ced5099
satoshi <angelsb94@gmail.com>
2020-04-23 10:53:43 -0500

    acabando secion

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,13 +3,52 @@ import React from 'react'
 import Header from '../components/Header'
 import Search from '../components/Search'
 
+import Categories from '../components/Categories'
+import Carousel from '../components/Carousel'
+import CarouselItem from '../components/CarouselItem'
+
+import Footer from '../components/Footer'
+
 //styles
 import '../assets/styles/App.scss'
+
 
 const App = () => (
   <div className="App">
     <Header/>
     <Search/>
+    
+    <Categories title="Mi lista">
+      <Carousel>
+        <CarouselItem/>
+        <CarouselItem/>
+        <CarouselItem/>
+        <CarouselItem/>
+        <CarouselItem/>
+      </Carousel>
+    </Categories>
+
+    <Categories title="Tendencias">
+      <Carousel>
+        <CarouselItem/>
+        <CarouselItem/>
+        <CarouselItem/>
+        <CarouselItem/>
+        <CarouselItem/>
+      </Carousel>
+    </Categories>
+
+    <Categories title="Originales de Platzi Video">
+      <Carousel>
+        <CarouselItem/>
+        <CarouselItem/>
+        <CarouselItem/>
+        <CarouselItem/>
+        <CarouselItem/>
+      </Carousel>
+    </Categories>
+
+    <Footer/>
   </div>
 )
 
